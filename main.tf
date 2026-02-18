0aee32767501d0840b22e724246dd26df7598500
@npks <anoop@dryad.net>
2026-02-18 21:34:13 +0100

    module added : vpc

--- main.tf
+++ main.tf
@@ -2,6 +2,22 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -16,6 +32,9 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
 
   tags = {
     Name = var.instance_name
